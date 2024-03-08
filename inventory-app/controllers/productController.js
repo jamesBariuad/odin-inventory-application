488adfd5c05da155ba27e5938d51a4fe3091de4d
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2024-03-09 06:49:00 +0800

    added todo controllers

---
+++ inventory-app/controllers/productController.js
@@ -0,0 +1,18 @@
+const Product = require("../models/product")
+const asyncHandler = require('express-async-handler')
+
+//display all products
+
+//display detail of a specific product
+
+//display product create form on GET
+
+//handle product create on POST
+
+//display product delete form on GET
+
+//handle product delete on POST
+
+//display product update form on GET
+
+//handle product update on POST
