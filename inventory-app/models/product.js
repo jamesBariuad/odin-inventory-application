b15e5213e2bcd84131a85b79e1739d32e862ada8
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2024-03-08 10:02:08 +0800

    added models

---
+++ inventory-app/models/product.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    name:{type:String, required:true, maxLength:100},
+    price:{type:Number, required:true},
+    description:{type:String, required:true},
+    numberInStock:{type:Number, required:true},
+    category:[{type: Schema.Types.ObjectId, ref:"Category"}]
+}) 
+
+
+ProductSchema.virtual("url").get(function(){
+    return `products/${this._id}`
+})
+
+module.exports = mongoose.model("Product", ProductSchema)
